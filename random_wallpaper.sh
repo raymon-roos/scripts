6ee3f8ced4b82135f1e3cd550a9b2fb697c7cb85
Raymon Roos <raymon.roos@student.rocva.nl>
2022-06-21 10:16:41 +0200

    chore: created .gitignore

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Select some random pictures to use as wallpapers
-bgMain=$(find "$HOME"/files/pictures/wallpapers/1920x1080/ -type f | shuf -n 1)
-bgLeft=$(find "$HOME"/files/pictures/wallpapers/1280x1024/ -type f | shuf -n 1)
-bgRight=$(find "$HOME"/files/pictures/wallpapers/1280x1024/ -type f | shuf -n 1)
+# Select a random picture to use as wallpaper
+wp=$(find "$HOME"/files/pictures/wallpapers/ -type f | shuf -n 1)
 
-# Feed images to bgs
-/usr/local/bin/bgs $bgLeft $bgMain $bgRight &
+# Feed image to bgs
+/usr/local/bin/bgs $wp &
