e387f807b4e4c917ea25daa626ae1f1a2deb7784
Raymon Roos <raymon.roos@hotmail.com>
2022-11-22 12:30:53 +0100

    refactor: tv_xrandr script now upscales output resolution

---
+++ tv_xrandr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output eDP1 --primary --auto --output HDMI2 --scale-from 1920x1080 --mode 4096x2160 --same-as eDP1
+
+# This xrandr command I use at school sometimes, to mirror laptop screen to a TV for presentations
