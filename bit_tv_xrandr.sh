5b60c940640b6ff081ac0862ec71121384956fba
Raymon Roos <raymon.roos@hotmail.com>
2022-12-10 19:11:02 +0100

    chore: stop tracking old file after rename

--- bit_tv_xrandr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xrandr --output eDP1 --primary --auto --output HDMI2 --mode 1920x1080 --same-as eDP1
-
-# This xrandr command I use at school sometimes, to mirror laptop screen to a TV for presentations
