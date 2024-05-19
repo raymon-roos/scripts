5c534ec9d44b3fd1812feecc9538e2a883ad1cde
Raymon Roos <raymon.roos@hotmail.com>
2024-05-19 14:27:36 +0200

    feat: rework `n` launch wrapper to spawn with an interactive shell
    When called, it replaces the shell used to run this script with an `st`
    process, that runs a new interactive shell, that runs an `nnn` process,
    which is then subsumed by a shell process once finished. All this to
    launch a terminal window with a specific cli application running, that
    can continue as a regular terminal window once the cli application is
    closed, without having dangling shells. This has the same result as
    manually spawning a new terminal, and calling `n`, but can be called
    from a hotkey or a launcher.

--- launch_nnn.sh
+++ launch_nnn.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ -f "$XDG_CONFIG_HOME/nnn/nnn_config" ]; then
-    source "$XDG_CONFIG_HOME/nnn/nnn_config"
-    n # Call the quitcd bash function 
-fi
-
-
-# This is just a sort of wrapper, to use with dwm
+exec st -c nnn -e bash -ic 'n; exec bash'
