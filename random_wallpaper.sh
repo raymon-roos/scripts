6c0ff61c88e9ceccf367f34698e44446f9353c8b
Raymon Roos <raymon.roos@hotmail.com>
2024-05-19 17:05:32 +0200

    !feat: adapt wallpaper "script" to new wallpaper location
    Turns it into the same one-liner I use in my `Xinitrc`

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Select some random pictures to use as wallpapers
-bgMain=$(find "$HOME"/files/pictures/wallpapers/1920x1080/ -type f | shuf -n 1)
-bgLeft=$(find "$HOME"/files/pictures/wallpapers/1280x1024/ -type f | shuf -n 1)
-bgRight=$(find "$HOME"/files/pictures/wallpapers/1280x1024/ -type f | shuf -n 1)
-
-# Feed images to bgs
-/usr/local/bin/bgs "$bgLeft" "$bgMain" "$bgRight" &
+exec bgs -z $(fd -tf '.' "$HOME/files/pictures/wallpapers/" | shuf -n 3)
