166b29c4bff8eeab108bf07bdafce283ce15704d
Raymon Roos <raymon.roos@hotmail.com>
2024-05-20 20:52:50 +0200

    feat: ditch some old "scripts"

--- mount_usb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-lsblk 
-
-read -p "usb device: (the part after /dev/ in /dev/sdxy) " USBDevice
-
-sudo mount -o gid=users,fmask=113,dmask=002 /dev/$USBDevice /mnt/usb/
-
-# For easily mounting usb, allowing writing by regular users
--- repo_fetcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Temporarily remember ssh key passphrase
-eval "$(ssh-agent -s)" && ssh-add -t 2m
-
-find . -name .git -execdir git fetch --all ';' &&
-find . -name .git -execdir git branch -vva ';' ;
-
-SSH_AGENT_PID="$(pidof ssh-agent)" ssh-agent -k
