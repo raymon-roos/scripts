e80f09cfe24345f2c0ac34e1a1f7e02fcde553b7
Raymon Roos <raymon.roos@hotmail.com>
2024-06-02 15:21:14 +0200

    feat: adapt `mailsync` script to work inside neomutt
    By adding an argument for setting the `isync` channel and guarding
    against launching multiple neomutt instances.

--- mailsync.sh
+++ mailsync.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
+# First argument should be an isync channel (account) name, defaults to all channels
+
 eval "$(keychain --dir "$XDG_STATE_HOME/keychain" --quiet --eval --agents 'gpg' 'secretray')"
 
 if [ -z "$(pgrep mbsync)" ] && [ -z "$(pgrep notmuch)" ]; then
 	(
 		eval "$(keychain --dir "$XDG_STATE_HOME/keychain" --quiet --noask --eval --agents 'gpg' 'secretray')"
-		mbsync -c "$XDG_CONFIG_HOME/isync/mbsyncrc" --all --quiet >/dev/null 2>&1
+		mbsync -c "$XDG_CONFIG_HOME/isync/mbsyncrc" "${1:---all}" --quiet >/dev/null 2>&1
 		notmuch new >/dev/null 2>&1
 		exit
 	) &
 fi
 
-exec neomutt
+[[ -z "$(pgrep neomutt)" ]] && exec neomutt
